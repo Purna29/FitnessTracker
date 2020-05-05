93095dcede5bff39e92b6dda94446c728fc7c4fc
Purna29 <vellalacheruvu.purna.29@gmail.com>
2020-05-05 15:23:14 -0400

    Social Sharing Done

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,14 @@ import App from './App.vue'
 import router from './router'
 import 'bulma';
 import '@fortawesome/fontawesome-free/css/all.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+
+var SocialSharing = require('vue-social-sharing');
+Vue.use(VueAxios, axios)
+Vue.axios.defaults.baseURL = "http://localhost:8000/api"
+Vue.use(SocialSharing);
 Vue.config.productionTip = false
 
 new Vue({
